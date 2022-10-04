a9866b6841fefee9cb3b8861e0dddb0b1f9118ba
alishudi <alexlishudi@gmail.com>
2022-10-05 01:46:55 +0300

    add metrics in utils.py

--- hw_asr/metric/utils.py
+++ hw_asr/metric/utils.py
@@ -1,10 +1,14 @@
 # Don't forget to support cases when target_text == ''
+from editdistance import distance
 
 def calc_cer(target_text, predicted_text) -> float:
-    # TODO: your code here
-    raise NotImplementedError()
+    if len(target_text) == 0:
+        return len(predicted_text)
+    return distance(target_text, predicted_text) / len(target_text)
+
 
 
 def calc_wer(target_text, predicted_text) -> float:
-    # TODO: your code here
-    raise NotImplementedError()
+    if len(target_text) == 0:
+        return len(predicted_text)
+    return distance(target_text.split(' '), predicted_text.split(' ')) / len(target_text.split(' '))
