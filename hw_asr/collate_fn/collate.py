50277b791708a0975ada17081220fbf1910904a3
alishudi <alexlishudi@gmail.com>
2022-10-05 01:31:16 +0300

    collate.py

--- hw_asr/collate_fn/collate.py
+++ hw_asr/collate_fn/collate.py
@@ -1,5 +1,7 @@
 import logging
 from typing import List
+from torch.nn.utils.rnn import pad_sequence
+import torch
 
 logger = logging.getLogger(__name__)
 
@@ -8,7 +10,17 @@ def collate_fn(dataset_items: List[dict]):
     """
     Collate and pad fields in dataset items
     """
-
+    
     result_batch = {}
-    # TODO: your code here
-    raise NotImplementedError
+
+    for key in dataset_items[0]:
+        if key not in ['spectrogram', 'text_encoded']:
+            result_batch[key] = [d[key] for d in dataset_items]
+        else:
+            result_batch[key] = [torch.squeeze(d[key], dim=0).T for d in dataset_items]
+            result_batch[key + '_length'] = torch.tensor([d[key].shape[-1] for d in dataset_items])
+
+    result_batch['spectrogram'] = pad_sequence(result_batch['spectrogram'], batch_first=True).transpose(1, 2)
+    result_batch['text_encoded'] = pad_sequence(result_batch['text_encoded'], batch_first=True)
+
+    return result_batch
