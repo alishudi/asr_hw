cd2d51fb7ce7fddf72d9179655b9e256e8c1679d
alishudi <alexlishudi@gmail.com>
2022-10-07 17:33:10 +0300

    fix sequential augmentation

--- hw_asr/batch_sampler/group_sort_batch_sampler.py
+++ hw_asr/batch_sampler/group_sort_batch_sampler.py
@@ -5,7 +5,8 @@ class GroupLengthBatchSampler(Sampler):
     def __init__(self, data_source, batch_size, batches_per_group=20):
         super().__init__(data_source)
         # TODO: your code here (optional)
-        
+        for sample in data_source:
+            sample['audio'].shape[0]
         raise NotImplementedError()
 
     def __iter__(self):
