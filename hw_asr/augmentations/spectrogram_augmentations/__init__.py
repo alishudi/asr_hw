582bb718e4ea2971302ee886ffbef11c5efde291
alishudi <alexlishudi@gmail.com>
2022-10-09 06:14:08 +0300

    fix augmentations

--- hw_asr/augmentations/spectrogram_augmentations/__init__.py
+++ hw_asr/augmentations/spectrogram_augmentations/__init__.py
@@ -2,6 +2,6 @@ from hw_asr.augmentations.wave_augmentations.FrequencyMask import FrequencyMask
 from hw_asr.augmentations.wave_augmentations.TimeMask import TimeMask
 
 __all__ = [
-    "GaussianNoise",
+    "FrequencyMask",
     "TimeMask"
 ]
