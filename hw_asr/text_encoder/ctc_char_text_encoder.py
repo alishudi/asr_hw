356d847fb32f5e3b452008f7f66e9e3a4228211b
alishudi <alexlishudi@gmail.com>
2022-10-13 20:00:29 +0300

    beam debug

--- hw_asr/text_encoder/ctc_char_text_encoder.py
+++ hw_asr/text_encoder/ctc_char_text_encoder.py
@@ -54,7 +54,6 @@ class CTCCharTextEncoder(CharTextEncoder):
         char_length, voc_size = probs.shape
         assert voc_size == len(self.ind2char)
 
-        print(probs[:probs_length].shape, probs[:probs_length])
-        beams = self.bs_lm.decode_beams(probs[:probs_length], beam_width=beam_size)
+        beams = self.bs_lm.decode_beams(probs[:probs_length].numpy(), beam_width=beam_size)
         hypos = [Hypothesis(text, combined_score) for text, _, _, _, combined_score in beams]
         return hypos
