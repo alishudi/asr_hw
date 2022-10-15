8c781063a65b857dcde6fe8cdd6660a92e7f6f86
alishudi <alexlishudi@gmail.com>
2022-10-15 03:43:28 +0300

    fix test

--- test.py
+++ test.py
@@ -66,7 +66,7 @@ def main(config, out_file):
                         "ground_trurh": batch["text"][i],
                         "pred_text_argmax": text_encoder.ctc_decode(argmax.cpu().numpy()),
                         "pred_text_beam_search": text_encoder.ctc_beam_search(
-                            batch["probs"][i], batch["log_probs_length"][i], beam_size=200
+                            batch["probs"][i].cpu().detach().numpy(), batch["log_probs_length"][i], beam_size=200
                         )[:10],
                     }
                 )
