3dd3ec35b2e968245a7f2a3053a456f5e07c49bc
alishudi <alexlishudi@gmail.com>
2022-10-16 21:29:03 +0300

    added new augmentation

---
+++ hw_asr/augmentations/wave_augmentations/BackgroundNoise.py
@@ -0,0 +1,55 @@
+import torch_audiomentations
+from torch import Tensor
+
+from hw_asr.utils import ROOT_PATH
+import gzip
+import os, shutil
+from speechbrain.utils.data_utils import download_file
+
+from hw_asr.augmentations.base import AugmentationBase
+
+URL_LINKS = {
+    "musan": "https://openslr.elda.org/resources/17/musan.tar.gz",
+    "rirs": "https://openslr.elda.org/resources/28/rirs_noises.zip"
+}
+
+#didnt test this augmentation properly, it might not work
+class BackgroundNoise(AugmentationBase):
+    def __init__(self, noise='rirs', *args, **kwargs):
+        self.path = self.prepare_noise(noise)
+        self._aug = torch_audiomentations.AddBackgroundNoise(background_paths=self.path, *args, **kwargs)
+
+    def __call__(self, data: Tensor):
+        try:
+            x = data.unsqueeze(1).numpy()
+            return Tensor(self._aug(x, sample_rate=16000)).squeeze(1)
+        except:
+            x = data.numpy()
+            return Tensor(self._aug(x, sample_rate=16000))
+
+    def prepare_noise(noise):
+        assert noise in URL_LINKS
+
+        data_dir = ROOT_PATH / "data" / noise
+        data_dir.mkdir(exist_ok=True, parents=True)
+        if noise == 'rirs':
+            gzip_path = data_dir / 'rirs_noises.zip'
+        else:
+            gzip_path = data_dir / 'musan.tar.gz'
+        if not os.path.exists(gzip_path):
+            print('Downloading the noise data.')
+            noise_url = URL_LINKS[noise]
+            download_file(noise_url, gzip_path)
+            print('Downloaded the noise data.')
+        
+        if noise == 'rirs':
+            unzipped_path = data_dir / 'rirs_noises'
+        else:
+            unzipped_path = data_dir / 'musan.tar'
+        if not os.path.exists(unzipped_path):
+            with gzip.open(gzip_path, 'rb') as f_zipped:
+                with open(unzipped_path, 'wb') as f_unzipped:
+                    shutil.copyfileobj(f_zipped, f_unzipped)
+            print('Unzipped the noise data.')
+
+        return unzipped_path
--- hw_asr/augmentations/wave_augmentations/__init__.py
+++ hw_asr/augmentations/wave_augmentations/__init__.py
@@ -1,9 +1,11 @@
 from hw_asr.augmentations.wave_augmentations.Gain import Gain
 from hw_asr.augmentations.wave_augmentations.GaussianNoise import GaussianNoise
 from hw_asr.augmentations.wave_augmentations.TimeStretch import TimeStretch
+from hw_asr.augmentations.wave_augmentations.BackgroundNoise import BackgroundNoise
 
 __all__ = [
     "Gain",
     "GaussianNoise",
-    "TimeStretch"
+    "TimeStretch",
+    "BackgroundNoise"
 ]
